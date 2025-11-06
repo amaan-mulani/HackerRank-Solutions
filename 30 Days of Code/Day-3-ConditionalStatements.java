b5385bc2dd8ff0f62e2bddfac71e8a31eacbcb07
Amaan Mulani <amaanmulani0109@gmail.com>
2025-11-06 08:05:50 +0530

    Added Day 3 solution
    Implement conditional statements to classify the integer input as 'Weird' or 'Not Weird' based on specified conditions.

---
+++ 30 Days of Code/Day-3-ConditionalStatements.java
@@ -0,0 +1,59 @@
+//Statements to be written or edited are enclosed in "****".
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+int main()
+{
+    string N_temp;
+    getline(cin, N_temp);
+
+    int N = stoi(ltrim(rtrim(N_temp)));
+  //write this code
+  //***************************************************
+    if(N % 2 != 0)
+    {
+        cout<<"Weird";
+    }
+    else if( N % 2 == 0 && (N>=2 && N<=5))
+    {
+        cout<<"Not Weird";
+    }
+    else if( N % 2 == 0 && (N>=6 && N<=20))
+    {
+        cout<<"Weird";
+    }
+    else if( N % 2 == 0 && N>20)
+    {
+        cout<<"Not Weird";
+    }
+  //****************************************************
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
