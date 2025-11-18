8cd0b6c2ce6bd1f2c7e7ea426c3fb864f5263138
Amaan Mulani <amaanmulani0109@gmail.com>
2025-11-18 16:44:35 +0530

    Update Day-12-Inheritance.java

--- 30 Days of Code/Day-12-Inheritance.java
+++ 30 Days of Code/Day-12-Inheritance.java
@@ -42,17 +42,14 @@ class Student extends Person
         int avg = sum/ testScores.length;
         
         if (avg >= 90 && avg <= 100)
-        {
-            return 'O';
-        } 
-        
-        else if (avg >= 90 && avg <= 100)
+        {return 'O';} 
+        else if (avg >= 80 && avg < 90)
         {return 'E';} 
-        else if (avg >= 70 && avg <= 80) 
+        else if (avg >= 70 && avg < 80) 
         {return 'A';}
-        else if (avg >= 55 && avg <= 70) 
+        else if (avg >= 55 && avg < 70) 
         {return 'P';}
-        else if (avg >= 40 && avg <= 55) 
+        else if (avg >= 40 && avg < 55) 
         {return 'D';}
         else 
         {return 'T';}
