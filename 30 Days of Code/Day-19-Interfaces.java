6a209dab7482c3394be33bdbac258df740cd9068
Amaan Mulani <amaanmulani0109@gmail.com>
2025-11-21 10:47:20 +0530

    Implement AdvancedArithmetic interface in Calculator

---
+++ 30 Days of Code/Day-19-Interfaces.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic
+{
+    int divisorSum(int n);
+}
+
+class Calculator implements AdvancedArithmetic
+{
+    @Override
+    public int divisorSum(int n)
+    {
+        int sum = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            if(n % i == 0)
+            {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // read the number
+        Calculator c = new Calculator();
+        System.out.println(("I implemented: AdvancedArithmetic"));
+        System.out.println(c.divisorSum(n));
+    }
+}
